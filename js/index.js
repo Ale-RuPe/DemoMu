6653b2880a635f06e6f06d88837de7386c7e4570
Alejandro Ruiz P <alejandrorupe_555@hotmail.com>
2020-01-17 01:08:57 -0600

    avances

--- js/index.js
+++ js/index.js
@@ -1,3 +1,50 @@
 $(document).ready(function(){
     $('.parallax').parallax();
+    $('.sidenav').sidenav();
+    ocultarElementos();
 });
+
+const ocultarElementos  = ()=> {
+    $( "#getDIV" ).hide();
+    $( "#getAllDIV" ).hide();
+}
+/*import * as C from './drawable/Components';
+import * as F from './functions';
+
+
+$(document).ready(function() {
+  initComponents();
+  initFiltroAlcaldias();
+  initFiltroTipoDelito();
+  initFiltroTipoArma();
+  F.initCluster();
+  $('select').formSelect();
+});
+*/
+/**
+ * Inicia los componentes de materialize (Barra de nav, Modales, Filtros)
+ */
+/*
+const initComponents = () => {
+  
+  C.map.zoomControl.setPosition('topright');
+  $('.collapsible').collapsible();  
+  $('.datepicker').datepicker();
+  $('.modal').modal();
+
+  $('#dateFilter_range').click( ()=>{
+    $('#modal1').modal('open');
+  });
+
+  $('.sidenav').sidenav();
+
+  $('#modal1').modal();
+
+  $(".selectFecha").datepicker({
+    format: 'dd/mm/yy'
+  });
+  $('#fechaIni').val('');
+  $('#fechaFin').val('');
+};*/
+
+
